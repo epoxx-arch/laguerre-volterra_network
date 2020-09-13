75ce3e22920e80cb555f2bea9c53136b264de0fb
Victor O. Costa <victor-o.c@hotmail.com>
2020-09-13 02:28:00 -0300

    Refactor apply_rlvn to use the same seed for all models in a run

--- apply_rlvn.py
+++ apply_rlvn.py
@@ -47,6 +47,7 @@ else:        # size == 'long'
     train_in, train_out = read_io('./signals_and_systems/long_' + order + '_train.csv')
     test_in, test_out   = read_io('./signals_and_systems/long_' + order + '_test.csv')
 
+## Configs
 # Structure
 L = 2; H = 3; Q = 5
 Fs = 25
@@ -55,199 +56,183 @@ wrange = 0.1
 # Laguerre smoothing constant
 alpha = 0.7
 # Number of runs
-ntimes = 30
-l2_regularization = False
+ntimes = 100
+l2_regularization = False # todo : regularization as a parameter of the script
 if l2_regularization:
     print('\nUSING L2 REGULARIZATION\n')
 
 print(f'(L,H,Q) = ({L},{H},{Q})')
 
-print('[WITHOUT IO_LINK / BO_LINK]')
+# RLVN with H maps from filter bank to hidden layer (same as original LVN)
 print('Non-extended (H maps from filter bank to hidden layer)')
 extended = False
-io_link = False
-bo_link = False
-model = RLVN(L, H, Q, Fs, extended, io_link, bo_link)
-
-train_errors = []
-test_errors = []
+# 2 boolean options: input-output direct e bank-output
+ifbf_test_errors = []
+itbf_test_errors = []
+ifbt_test_errors = []
+itbt_test_errors = []
+ifbf_train_errors = []
+itbf_train_errors = []
+ifbt_train_errors = []
+itbt_train_errors = []
 for _ in range(ntimes):
-    model.randomize_weights(weights_range = wrange)
+    # All models use the same seed in a run
+    seed = np.random.randint(1000)
+    
+    # Without io / bo links
+    io_link = False
+    bo_link = False
+    # Train model and predict outputs
+    model = RLVN(L, H, Q, Fs, extended, io_link, bo_link)
+    model.randomize_weights(weights_range = wrange, seed = seed)
     model.train(in_signal = train_in, out_signal = train_out, alpha = alpha, l2_regularization = l2_regularization)
     estimated_train_out = model.predict(in_signal = train_in)
     estimated_test_out = model.predict(in_signal = test_in)
-
+    # Comput and keep errors
     nmse_train = NMSE(train_out, estimated_train_out, alpha)
     nmse_test = NMSE(test_out, estimated_test_out, alpha)
-    train_errors.append(nmse_train)
-    test_errors.append(nmse_test)
+    ifbf_train_errors.append(nmse_train)
+    ifbf_test_errors.append(nmse_test)
     
-print(f'NMSE train: {np.mean(train_errors)} {np.std(train_errors)}')
-print(f'NMSE  test: {np.mean(test_errors)} {np.std(test_errors)}')
-
-print('Extended (HQ maps from filter bank to hidden layer)')
-extended = True
-io_link = False
-bo_link = False
-model = RLVN(L, H, Q, Fs, extended, io_link, bo_link)
-
-train_errors = []
-test_errors = []
-for _ in range(ntimes):
-    model.randomize_weights(weights_range = wrange)
-    model.train(in_signal = train_in, out_signal = train_out, alpha = alpha, l2_regularization = l2_regularization)
-    
-    estimated_train_out = model.predict(in_signal = train_in)
-    estimated_test_out =  model.predict(in_signal = test_in)
-    
-    nmse_train = NMSE(train_out, estimated_train_out, alpha)
-    nmse_test = NMSE(test_out, estimated_test_out, alpha)
-    train_errors.append(nmse_train)
-    test_errors.append(nmse_test)
-        
-print(f'NMSE train: {np.mean(train_errors)} {np.std(train_errors)}')
-print(f'NMSE  test: {np.mean(test_errors)} {np.std(test_errors)}')
-
-
-#######################
-print('[WITH IO_LINK / WITHOUT BO_LINK]')
-print('Non-extended (H maps from filter bank to hidden layer)')
-extended = False
-io_link = True
-bo_link = False
-model = RLVN(L, H, Q, Fs, extended, io_link, bo_link)
-
-train_errors = []
-test_errors = []
-for _ in range(ntimes):
-    model.randomize_weights(weights_range = wrange)
+    # With io / without bo links
+    io_link = True
+    bo_link = False
+    # Train model and predict outputs
+    model = RLVN(L, H, Q, Fs, extended, io_link, bo_link)
+    model.randomize_weights(weights_range = wrange, seed = seed)
     model.train(in_signal = train_in, out_signal = train_out, alpha = alpha, l2_regularization = l2_regularization)
     estimated_train_out = model.predict(in_signal = train_in)
     estimated_test_out = model.predict(in_signal = test_in)
-
+    # Comput and keep errors
     nmse_train = NMSE(train_out, estimated_train_out, alpha)
     nmse_test = NMSE(test_out, estimated_test_out, alpha)
-    train_errors.append(nmse_train)
-    test_errors.append(nmse_test)
+    itbf_train_errors.append(nmse_train)
+    itbf_test_errors.append(nmse_test)
     
-print(f'NMSE train: {np.mean(train_errors)} {np.std(train_errors)}')
-print(f'NMSE  test: {np.mean(test_errors)} {np.std(test_errors)}')
-
-print('Extended (HQ maps from filter bank to hidden layer)')
-extended = True
-io_link = True
-bo_link = False
-model = RLVN(L, H, Q, Fs, extended, io_link, bo_link)
-
-train_errors = []
-test_errors = []
-for _ in range(ntimes):
-    model.randomize_weights(weights_range = wrange)
-    model.train(in_signal = train_in, out_signal = train_out, alpha = alpha, l2_regularization = l2_regularization)
-    
-    estimated_train_out = model.predict(in_signal = train_in)
-    estimated_test_out =  model.predict(in_signal = test_in)
-    
-    nmse_train = NMSE(train_out, estimated_train_out, alpha)
-    nmse_test = NMSE(test_out, estimated_test_out, alpha)
-    train_errors.append(nmse_train)
-    test_errors.append(nmse_test)
-        
-print(f'NMSE train: {np.mean(train_errors)} {np.std(train_errors)}')
-print(f'NMSE  test: {np.mean(test_errors)} {np.std(test_errors)}')
-
-
-# #######################
-print('[WITHOUT IO_LINK / WITH BO_LINK]')
-print('Non-extended (H maps from filter bank to hidden layer)')
-extended = False
-io_link = False
-bo_link = True
-model = RLVN(L, H, Q, Fs, extended, io_link, bo_link)
-
-train_errors = []
-test_errors = []
-for _ in range(ntimes):
-    model.randomize_weights(weights_range = wrange)
+    # Without io / with bo links
+    io_link = False
+    bo_link = True
+    # Train model and predict outputs
+    model = RLVN(L, H, Q, Fs, extended, io_link, bo_link)
+    model.randomize_weights(weights_range = wrange, seed = seed)
     model.train(in_signal = train_in, out_signal = train_out, alpha = alpha, l2_regularization = l2_regularization)
     estimated_train_out = model.predict(in_signal = train_in)
     estimated_test_out = model.predict(in_signal = test_in)
-
+    # Comput and keep errors
     nmse_train = NMSE(train_out, estimated_train_out, alpha)
     nmse_test = NMSE(test_out, estimated_test_out, alpha)
-    train_errors.append(nmse_train)
-    test_errors.append(nmse_test)
+    ifbt_train_errors.append(nmse_train)
+    ifbt_test_errors.append(nmse_test)
     
-print(f'NMSE train: {np.mean(train_errors)} {np.std(train_errors)}')
-print(f'NMSE  test: {np.mean(test_errors)} {np.std(test_errors)}')
-
-print('Extended (HQ maps from filter bank to hidden layer)')
-extended = True
-io_link = False
-bo_link = True
-model = RLVN(L, H, Q, Fs, extended, io_link, bo_link)
-
-train_errors = []
-test_errors = []
-for _ in range(ntimes):
-    model.randomize_weights(weights_range = wrange)
-    model.train(in_signal = train_in, out_signal = train_out, alpha = alpha, l2_regularization = l2_regularization)
-    
-    estimated_train_out = model.predict(in_signal = train_in)
-    estimated_test_out =  model.predict(in_signal = test_in)
-    
-    nmse_train = NMSE(train_out, estimated_train_out, alpha)
-    nmse_test = NMSE(test_out, estimated_test_out, alpha)
-    train_errors.append(nmse_train)
-    test_errors.append(nmse_test)
-        
-print(f'NMSE train: {np.mean(train_errors)} {np.std(train_errors)}')
-print(f'NMSE  test: {np.mean(test_errors)} {np.std(test_errors)}')
-
-
-#######################
-print('[WITH IO_LINK / BO_LINK]')
-print('Non-extended (H maps from filter bank to hidden layer)')
-extended = False
-io_link = True
-bo_link = True
-model = RLVN(L, H, Q, Fs, extended, io_link, bo_link)
-
-train_errors = []
-test_errors = []
-for _ in range(ntimes):
-    model.randomize_weights(weights_range = wrange)
+    # With io / bo links
+    io_link = True
+    bo_link = True
+    # Train model and predict outputs
+    model = RLVN(L, H, Q, Fs, extended, io_link, bo_link)
+    model.randomize_weights(weights_range = wrange, seed = seed)
     model.train(in_signal = train_in, out_signal = train_out, alpha = alpha, l2_regularization = l2_regularization)
     estimated_train_out = model.predict(in_signal = train_in)
     estimated_test_out = model.predict(in_signal = test_in)
-
+    # Comput and keep errors
     nmse_train = NMSE(train_out, estimated_train_out, alpha)
     nmse_test = NMSE(test_out, estimated_test_out, alpha)
-    train_errors.append(nmse_train)
-    test_errors.append(nmse_test)
-    
-print(f'NMSE train: {np.mean(train_errors)} {np.std(train_errors)}')
-print(f'NMSE  test: {np.mean(test_errors)} {np.std(test_errors)}')
+    itbt_train_errors.append(nmse_train)
+    itbt_test_errors.append(nmse_test)
 
-print('Extended (HQ maps from filter bank to hidden layer)')
-extended = True
-io_link = True
-bo_link = True
-model = RLVN(L, H, Q, Fs, extended, io_link, bo_link)
+print('Train')
+print(f'NMSE without io/bo:      {np.mean(ifbf_train_errors)} ({np.std(ifbf_train_errors)})')
+print(f'NMSE with io/without bo: {np.mean(itbf_train_errors)} ({np.std(itbf_train_errors)})')
+print(f'NMSE without io/with bo: {np.mean(ifbt_train_errors)} ({np.std(ifbt_train_errors)})')
+print(f'NMSE with io/bo:         {np.mean(itbt_train_errors)} ({np.std(itbt_train_errors)})')
+print('Test')
+print(f'NMSE without io/bo:      {np.mean(ifbf_test_errors)} ({np.std(ifbf_test_errors)})')
+print(f'NMSE with io/without bo: {np.mean(itbf_test_errors)} ({np.std(itbf_test_errors)})')
+print(f'NMSE without io/with bo: {np.mean(ifbt_test_errors)} ({np.std(ifbt_test_errors)})')
+print(f'NMSE with io/bo:         {np.mean(itbt_test_errors)} ({np.std(itbt_test_errors)})')
 
-train_errors = []
-test_errors = []
+# RLVN with HQ maps from bank to hidden layer, using multidimensional polynomials as units
+print('\nExtended RLVN (HQ maps from filter bank to hidden layer)')
+extended = False
+# 2 boolean options: input-output direct e bank-output
+ifbf_test_errors = []
+itbf_test_errors = []
+ifbt_test_errors = []
+itbt_test_errors = []
+ifbf_train_errors = []
+itbf_train_errors = []
+ifbt_train_errors = []
+itbt_train_errors = []
 for _ in range(ntimes):
-    model.randomize_weights(weights_range = wrange)
+    # All models use the same seed in a run
+    seed = np.random.randint(1000)
+    
+    # Without io / bo links
+    io_link = False
+    bo_link = False
+    # Train model and predict outputs
+    model = RLVN(L, H, Q, Fs, extended, io_link, bo_link)
+    model.randomize_weights(weights_range = wrange, seed = seed)
     model.train(in_signal = train_in, out_signal = train_out, alpha = alpha, l2_regularization = l2_regularization)
-    
     estimated_train_out = model.predict(in_signal = train_in)
-    estimated_test_out =  model.predict(in_signal = test_in)
-    
+    estimated_test_out = model.predict(in_signal = test_in)
+    # Comput and keep errors
     nmse_train = NMSE(train_out, estimated_train_out, alpha)
     nmse_test = NMSE(test_out, estimated_test_out, alpha)
-    train_errors.append(nmse_train)
-    test_errors.append(nmse_test)
-        
-print(f'NMSE train: {np.mean(train_errors)} {np.std(train_errors)}')
-print(f'NMSE  test: {np.mean(test_errors)} {np.std(test_errors)}')
+    ifbf_train_errors.append(nmse_train)
+    ifbf_test_errors.append(nmse_test)
+    
+    # With io / without bo links
+    io_link = True
+    bo_link = False
+    # Train model and predict outputs
+    model = RLVN(L, H, Q, Fs, extended, io_link, bo_link)
+    model.randomize_weights(weights_range = wrange, seed = seed)
+    model.train(in_signal = train_in, out_signal = train_out, alpha = alpha, l2_regularization = l2_regularization)
+    estimated_train_out = model.predict(in_signal = train_in)
+    estimated_test_out = model.predict(in_signal = test_in)
+    # Comput and keep errors
+    nmse_train = NMSE(train_out, estimated_train_out, alpha)
+    nmse_test = NMSE(test_out, estimated_test_out, alpha)
+    itbf_train_errors.append(nmse_train)
+    itbf_test_errors.append(nmse_test)
+    
+    # Without io / with bo links
+    io_link = False
+    bo_link = True
+    # Train model and predict outputs
+    model = RLVN(L, H, Q, Fs, extended, io_link, bo_link)
+    model.randomize_weights(weights_range = wrange, seed = seed)
+    model.train(in_signal = train_in, out_signal = train_out, alpha = alpha, l2_regularization = l2_regularization)
+    estimated_train_out = model.predict(in_signal = train_in)
+    estimated_test_out = model.predict(in_signal = test_in)
+    # Comput and keep errors
+    nmse_train = NMSE(train_out, estimated_train_out, alpha)
+    nmse_test = NMSE(test_out, estimated_test_out, alpha)
+    ifbt_train_errors.append(nmse_train)
+    ifbt_test_errors.append(nmse_test)
+    
+    # With io / bo links
+    io_link = True
+    bo_link = True
+    # Train model and predict outputs
+    model = RLVN(L, H, Q, Fs, extended, io_link, bo_link)
+    model.randomize_weights(weights_range = wrange, seed = seed)
+    model.train(in_signal = train_in, out_signal = train_out, alpha = alpha, l2_regularization = l2_regularization)
+    estimated_train_out = model.predict(in_signal = train_in)
+    estimated_test_out = model.predict(in_signal = test_in)
+    # Comput and keep errors
+    nmse_train = NMSE(train_out, estimated_train_out, alpha)
+    nmse_test = NMSE(test_out, estimated_test_out, alpha)
+    itbt_train_errors.append(nmse_train)
+    itbt_test_errors.append(nmse_test)
+
+print('Train')
+print(f'NMSE without io/bo:      {np.mean(ifbf_train_errors)} ({np.std(ifbf_train_errors)})')
+print(f'NMSE with io/without bo: {np.mean(itbf_train_errors)} ({np.std(itbf_train_errors)})')
+print(f'NMSE without io/with bo: {np.mean(ifbt_train_errors)} ({np.std(ifbt_train_errors)})')
+print(f'NMSE with io/bo:         {np.mean(itbt_train_errors)} ({np.std(itbt_train_errors)})')
+print('Test')
+print(f'NMSE without io/bo:      {np.mean(ifbf_test_errors)} ({np.std(ifbf_test_errors)})')
+print(f'NMSE with io/without bo: {np.mean(itbf_test_errors)} ({np.std(itbf_test_errors)})')
+print(f'NMSE without io/with bo: {np.mean(ifbt_test_errors)} ({np.std(ifbt_test_errors)})')
+print(f'NMSE with io/bo:         {np.mean(itbt_test_errors)} ({np.std(itbt_test_errors)})')
