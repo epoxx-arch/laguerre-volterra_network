28fb52407df3db020955fc85a0e2999a81a8652b
Victor O. Costa <victor-o.c@hotmail.com>
2023-06-25 22:42:47 -0300

    Update license year

--- simulated_systems.py
+++ simulated_systems.py
@@ -1,6 +1,6 @@
 #!python3
 
-# Copyright (C) 2020  Victor O. Costa
+# Copyright (C) 2023  Victor O. Costa
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
