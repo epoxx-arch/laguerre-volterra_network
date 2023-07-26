0b671058cf3ce48811c0a71c729cba47a122f82b
Victor O. Costa <victor-o.c@hotmail.com>
2023-07-25 23:44:03 -0300

    Parameterize strategies comparison plotting to get mean, median or minimum plots

--- strategy_comparison_plot_results.py
+++ strategy_comparison_plot_results.py
@@ -21,6 +21,23 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
+if len(sys.argv) != 2:
+    print('Error, inform the statistic to be plotted')
+    exit(-1)
+
+stat = sys.argv[1]
+
+if stat != 'mean' and stat != 'median' and stat != 'minimum':
+    print('Error, statistic should be \'mean\', \'median\' or \'minimum\'')
+    exit(-1)
+
+if stat == 'mean':
+    ylabel = 'Erro médio (EQMN)'
+elif stat == 'median':
+    ylabel = 'Erro mediano (EQMN)'
+else: 
+    ylabel = 'Erro mínimo (EQMN)'
+
 function_evals = np.arange(100, 10100, 100)
 ntimes = 30
 
@@ -29,20 +46,27 @@ dataset_orders = ['finite', 'infinite']
 dataset_partitions = ['train', 'test']
 color_set = ['#ea5545', '#87bc45', '#27aeef']
 
-for partition in dataset_partitions:
-    for system_order in dataset_orders:
+for system_order in dataset_orders:
+    for partition in dataset_partitions:
         plt.figure()
         for index, strategy in enumerate(optimization_strategies):
             base_filename   = './data/strategy_' + strategy + '_' + system_order + '_' + partition
             costs_matrix  = np.load(base_filename + '_costs.npy')
             
             # Plot average cost history for the given metaheuristic
-            average_cost_history = np.sum(costs_matrix, axis=0) / ntimes
-            plt.plot(function_evals, average_cost_history, label='Estratégia ' + strategy, linewidth=3, color=color_set[index])
+            if stat == 'mean':
+                cost_history_stat = np.mean(costs_matrix, axis=0)
+            elif stat == 'median':
+                cost_history_stat = np.median(costs_matrix, axis=0)
+            else:
+                cost_history_stat = np.min(costs_matrix, axis=0)
+            
+            
+            plt.plot(function_evals, cost_history_stat, label='Estratégia ' + strategy, linewidth=3, color=color_set[index])
 
-        plt.title(partition + '_partition_' + system_order + '_order')
+        plt.title(stat + '_' + partition + '_' + system_order)
         plt.xlabel('AFO', fontsize=18)
-        plt.ylabel('Erro médio (EQMN)', fontsize=18)
+        plt.ylabel(ylabel, fontsize=18)
         plt.legend(fontsize=16)
 
         plt.show()
